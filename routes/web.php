539903f7ec5228f41eda93c066db3107f102bf7f
Van Meerbergen Renaud <113695652+VanMeerbergenRenaud@users.noreply.github.com>
2023-10-12 18:47:47 +0200

    update of the routes

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 Route::get('/about', [AboutController::class, 'index'])->name('about');
 Route::get('/contact', [ContactController::class, 'index'])->name('contact');
